3c7b0f12e311a6a50d6379f8485442db95122c55
Sam Goldman <samwgoldman@gmail.com>
2013-08-30 20:31:44 -0400

    Don't need to declare this MIME type as of Rails 3.2
    Anyone consuming this library on an older version of rails can define
    the MIME type in an initializer.

--- lib/princely/railtie.rb
+++ lib/princely/railtie.rb
@@ -4,10 +4,6 @@ require 'action_dispatch/http/mime_types'
 module Princely
   class Railtie < Rails::Railtie
 
-    initializer :init_mime_types do
-      Mime::Type.register 'application/pdf', :pdf
-    end
-
     initializer :insert_into_action_controller do
       ActiveSupport.on_load :action_controller  do
         ActionController::Base.send(:include, Princely::PdfHelper)
